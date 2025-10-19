60f965ad0a500e7bd469edc0012888d1c6d38e6e
BearForOdin <selasidzeamesi82@gmail.com>
2025-10-19 16:53:38 -0700

    taskdone

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,10 @@
--- File: task_5.sql
--- Inserts a single row into the customer table in the alx_book_store database
+-- File: task_6.sql
+-- Inserts multiple rows into the customer table in the alx_book_store database
 
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
